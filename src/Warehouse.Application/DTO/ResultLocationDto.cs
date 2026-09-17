0220e5e21a404809769cd27156f2b4cedda2d43b
dogelkowy <michalek.miki2008@gmail.com>
2026-09-17 18:45:51 +0200

    Add Missing Locations CRUD From branch locations-page

---
+++ src/Warehouse.Application/DTO/ResultLocationDto.cs
@@ -0,0 +1,6 @@
+namespace Warehouse.Application.DTO;
+
+public class ResultLocationDto
+{
+    
+}
---
+++ src/Warehouse.Application/Validator/AddLocationValidator.cs
@@ -0,0 +1,6 @@
+namespace Warehouse.Application.Validator;
+
+public class AddLocationValidator
+{
+    
+}
